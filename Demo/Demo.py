81717f8d0ec282f78cc8af944eaae56d83118be9
Rodolfo <rodolfo.saldanha@hotmail.com>
2016-06-24 20:54:43 -0700

    Hard coding on the transformation of the tables into classes

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -1,4 +1,4 @@
-import pypyodbc
+"""import pypyodbc
 # Database connection
 print('Trying to connect')
 try:
@@ -9,23 +9,73 @@ try:
 	print('Connected')
 
 except:
-	print('Could not connect')
+	print('Could not connect')"""
 
 
-"""
+
 def main():
-	tableName = raw_input('Table name: ')
-	createClass(tableName)
+	tableName = input('Table name: ')
 
-	
-def createClass(tableName):
+	if tableName == 'AcctHist':
+		createClassAcctHist()
+	elif tableName == 'ADM_INPT':
+		createClassAdm_inpt()
+	elif tableName == 'BURNS': 
+		createClassBurns()
+	elif tableName == 'comments': 
+		createClassComments() 
+	elif tableName == 'CULTURE': 
+		createClassCulture()
+	elif tableName == 'dellog': 
+		createClassDellog()
+	elif tableName == 'DIAGS': 
+		createClassDiags()
+	elif tableName == 'EMERG':
+	    createClassEmerg()
+	elif tableName == 'FINANCE': 
+		createClassFinance()
+	elif tableName == 'FLDDETAI': 
+		createClassFlddetai()
+	elif tableName == 'GENMACH': 
+		createClassGenmach()
+	elif tableName == 'HEMO': 
+		createClassHemo()
+	elif tableName == 'HOSPREV':
+		createClassHosprev()
+	elif tableName == 'ICU':
+		createClassIcu()
+	elif tableName == 'INJDETS':
+		createClassInjdets()
+	elif tableName == 'INJDIAG':
+		createClassInjdiag()
+	elif tableName == 'INJMECH':
+		createClassInjmech()
+	elif tableName == 'LAB':
+		createClassLab()
+	elif tableName == 'MAINDATA':
+		createClassMaindata()
+	elif tableName == 'MORTDETS':
+		createClassMortdets()
+	elif tableName == 'MTOS':
+		createClassMtos()
+	elif tableName == 'NARRATIV':
+		createClassNarrativ()
+	elif tableName == 'OPRM':
+		createClassOprm()
+	elif tableName == 'ORGANS':
+		create
+
+def createClassAcctHist():
     # Creating class
-	class 'tableName':
+    dois = 'falou'
+    print (dois)
+
+"""class AcctHist:
 				def __init__(self, patientId, task, timestamp, otherAttributes):
 					self.patientId = patientId
 					self.task = task
 					self.timestamp = timestamp
-					otherAttributes = otherAttributes
+					self.otherAttributes = otherAttributes
 
     myCursor = myConnection.cursor()
 	SQLSelectCommand = "SELECT * \
@@ -46,9 +96,8 @@ def createClass(tableName):
 
 def filter():
 
-def output2File():
+def output2File():"""
 
 
 if __name__ == '__main__':
 	main()
-"""
