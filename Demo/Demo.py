557e4b4cb7f875b7017ce4df3dec9f3cc1a8d69f
Rodolfo <rodolfo.saldanha@hotmail.com>
2016-06-27 14:54:43 -0700

    scope of the classes adjusted and ADM_INPT added

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -1,134 +1,126 @@
-"""import pypyodbc
-# Database connection
-print('Trying to connect')
-try:
-	myConnection = pypyodbc.connect('Driver={SQL Server};'
-									'Server=localhost;'
-									'Database=Trauma2;'
-									'uid=Rodolfo;pwd=Trauma2')
-	print('Connected')
-
-except:
-	print('Could not connect')"""
-
-
-
 def main():
 	tableName = input('Table name: ')
 
 	if tableName == 'AcctHist':
-		createClassAcctHist()
+		getTuplesFromAcctHist()
 	elif tableName == 'ADM_INPT':
-		createClassAdm_inpt()
+		getTuplesFromAdm_inpt()
 	elif tableName == 'BURNS': 
-		createClassBurns()
+		getTuplesFromBurns()
 	elif tableName == 'comments': 
-		createClassComments() 
+		getTuplesFromComments() 
 	elif tableName == 'CULTURE': 
-		createClassCulture()
+		getTuplesFromCulture()
 	elif tableName == 'dellog': 
-		createClassDellog()
+		getTuplesFromDellog()
 	elif tableName == 'DIAGS': 
-		createClassDiags()
+		getTuplesFromDiags()
 	elif tableName == 'EMERG':
-	    createClassEmerg()
+	    getTuplesFromEmerg()
 	elif tableName == 'FINANCE': 
-		createClassFinance()
+		getTuplesFromFinance()
 	elif tableName == 'FLDDETAI': 
-		createClassFlddetai()
+		getTuplesFromFlddetai()
 	elif tableName == 'GENMACH': 
-		createClassGenmach()
+		getTuplesFromGenmach()
 	elif tableName == 'HEMO': 
-		createClassHemo()
+		getTuplesFromHemo()
 	elif tableName == 'HOSPREV':
-		createClassHosprev()
+		getTuplesFromHosprev()
 	elif tableName == 'ICU':
-		createClassIcu()
+		getTuplesFromIcu()
 	elif tableName == 'INJDETS':
-		createClassInjdets()
+		getTuplesFromInjdets()
 	elif tableName == 'INJDIAG':
-		createClassInjdiag()
+		getTuplesFromInjdiag()
 	elif tableName == 'INJMECH':
-		createClassInjmech()
+		getTuplesFromInjmech()
 	elif tableName == 'LAB':
-		createClassLab()
+		getTuplesFromLab()
 	elif tableName == 'MAINDATA':
-		createClassMaindata()
+		getTuplesFromMaindata()
 	elif tableName == 'MORTDETS':
-		createClassMortdets()
+		getTuplesFromMortdets()
 	elif tableName == 'MTOS':
-		createClassMtos()
+		getTuplesFromMtos()
 	elif tableName == 'NARRATIV':
-		createClassNarrativ()
+		getTuplesFromNarrativ()
 	elif tableName == 'OPRM':
-		createClassOprm()
+		getTuplesFromOprm()
 	elif tableName == 'ORGANS':
-		createClassOrgans()
+		getTuplesFromOrgans()
 	elif tableName == 'PERHIST':
-		createClassPerhist()
+		getTuplesFromPerhist()
 	elif tableName == 'POSTHOSP':
-		createClassPosthosp()
+		getTuplesFromPosthosp()
 	elif tableName == 'PRECONDS':
-		createClassPreconds()
+		getTuplesFromPreconds()
 	elif tableName == 'PROTECT':
-		createClassProtect()
+		getTuplesFromProtect()
 	elif tableName == 'QAISSUE':
-		createClassQaissue()
+		getTuplesFromQaissue()
 	elif tableName == 'RADIOLOG':
-		createClassRadiolog()
+		getTuplesFromRadiolog()
 	elif tableName == 'READMIT':
-		createClassReadmit()
+		getTuplesFromReadmit()
 	elif tableName == 'STEP':
-		createClassStep()
+		getTuplesFromStep()
 	elif tableName == 'SURG':
-		createClassSurg()
+		getTuplesFromSurg()
 	elif tableName == 'sysdefs':
-		createClassSysdefs()
+		getTuplesFromSysdefs()
 	elif tableName == 'TLogComm':
-		createClassTLogComm()
+		getTuplesFromTLogComm()
 	elif tableName == 'TOXIANAL':
-		createClassToxianal()
+		getTuplesFromToxianal()
 	elif tableName == 'TRA':
-		createClassTra()
+		getTuplesFromTra()
 	elif tableName == 'tranlog':
-		createClassTranlog()
+		getTuplesFromTranlog()
 	elif tableName == 'TRANSFER':
-		createClassTransfer()
+		getTuplesFromTransfer()
 	elif tableName == 'TRANSPRT':
-		createClassTransprt()
+		getTuplesFromTransprt()
 	elif tableName == 'TREATMEN':
-		createClassTreatmen()
+		getTuplesFromTreatmen()
 	elif tableName == 'TRICRIT':
-		createClassTricrit()
+		getTuplesFromTricrit()
 	elif tableName == 'TRMTEAM':
-		createClassTrmteam()
+		getTuplesFromTrmteam()
 	elif tableName == 'TTDETLS':
-		createClassTtdetls()
+		getTuplesFromTtdetls()
 	elif tableName == 'VITALS':
-		createClassVitals()
+		getTuplesFromVitals()
 	elif tableName == 'WARD':
-		createClassWard()
+		getTuplesFromWard()
 	else: 
 		print ('Type a valid table name.\n')
 
 	myConnection.close()
 	myCursor.close()
 
-def createClassAcctHist():
-    # Creating class
-    dois = 'falou'
-    print (dois)
+def getTuplesFromAcctHist():
+	print ('Testing AcctHist!') 
+	myCursor = myConnection.cursor()
+	SQLSelectCommand = "SELECT * \
+						FROM TRAUMA2.AcctHist"
+	# Getting tuples
+	numberOfTuples = 0
+	try:
+		myCursor.execute(SQLSelectCommand)
+		results = myCursor.fetchall()
+		for row in results:
+			patient[numberOfTuples] = Tuples(str(row[0]), 'AcctHist', str(row[2]), 'acctno = ' + str(row[1]) + ', acttime = ' + str(row[3]) + ', action = ' + str(row[4]) + ', acuser = ' + str(row[5]) + ', acstation = ' + str(row[6]))
+			numberOfTuples = numberOfTuples + 1
+	except:
+		print ("ERROR: unable to fetch data")
 
-"""class AcctHist:
-		def __init__(self, patientId, task, timestamp, otherAttributes):
-			self.patientId = patientId
-			self.task = task
-			self.timestamp = timestamp
-			self.otherAttributes = otherAttributes
+def getTuplesFromAdm_inpt():
+	print('Testing ADM_INPT!') 
 
 	myCursor = myConnection.cursor()
 	SQLSelectCommand = "SELECT * \
-						FROM TRAUMA2.AcctHist" 
+						FROM TR02_TRANS.ADM_INPT" 
 	
     # Getting tuples
 	numberOfTuples = 0
@@ -136,19 +128,51 @@ def createClassAcctHist():
 		myCursor.execute(SQLSelectCommand)
 		results = myCursor.fetchall()
 		for row in results:
-			patient[numberOfTuples] = AcctHist()
-			patient[numberOfTuples].patientId = str(row[0])
-			patient[numberOfTuples].timestamp = str(row[2])
-			patient[numberOfTuples].task = ?????
-			patient[numberOfTuples].otherAttributes = 'acctno = "' + str(row[1]) + '", acttime = "' + str(row[3]) + '", action = "' + str(row[4]) + '", acuser = "' + str(row[5]) + '", acstation = "' + str(row[6]) + '"'
-			numberOfTuples++
+			patient[numberOfTuples] = Tuples(str(row[34]), 'ADM_INPT', str(row[2]), 'PT_LOCAT = ' + str(row[0]) + ', ROOM_NO = ' + str(row[1]) + ', DATE_OUT = ' + str(row[3]) + ', PROVDER_IN = ' + str(row[4]) + ', LOS_IN = ' + str(row[5]) + ', CAREPHASE = ' + str(row[6]) + ', DUMFLD1 = ' + str(row[7]) + ', DUMFLD2 = ' + str(row[8]) + ', DUMFLD3 = ' + str(row[9]) + ', DUMFLD4 = ' + str(row[10]) + ', DUMFLD5 = ' + str(row[11]) + ', DUMFLD6 = ' + str(row[12]) + ', DUMFLD7 = ' + str(row[13]) + ', DUMFLD8 = ' + str(row[14]) + ', DUMFLD9 = ' + str(row[15]) + ', DUMFLD10 = ' + str(row[16]) + ', DUMFLD11 = ' + str(row[17]) + ', DUMFLD12 = ' + str(row[18]) + ', DUMFLD13 = ' + str(row[19]) + ', DUMFLD14 = ' + str(row[20]) + ', DUMFLD15 = ' + str(row[21]) + ', DUMFLD16 = ' + str(row[22]) + ', DUMFLD17 = ' + str(row[23]) + ', DUMFLD18 = ' + str(row[24]) + ', DUMFLD19 = ' + str(row[25]) + ', DUMFLD20 = ' + str(row[26]) + ', INPT_SER = ' + str(row[27]) + ', ACCTNO = ' + str(row[28]),  + ', DE_STATUS = ' + str(row[29]),  + ', DECOMMFLAG = ' + str(row[30]) + ', PARENTID = ' + str(row[31]) + ', PARENTREC = ' + str(row[32]),  + ', COPYNO = ' + str(row[33]))
+			numberOfTuples = numberOfTuples + 1
 	except:
-		print "ERROR: unable to fetch data"
+		print ("ERROR: unable to fetch data")
 
+def getTuplesFromBurns():
+	print('Testing BURNS!') 
+
+	myCursor = myConnection.cursor()
+	SQLSelectCommand = "SELECT * \
+						FROM TR02_TRANS.BURNS" 
+	
+    # Getting tuples
+	numberOfTuples = 0
+	try:
+		myCursor.execute(SQLSelectCommand)
+		results = myCursor.fetchall()
+		for row in results:
+			patient[numberOfTuples] = Tuples(str(row[30]), 'BURNS', str(row[2]), 'PT_LOCAT = ' + str(row[0]) + ', ROOM_NO = ' + str(row[1]) + ', DATE_OUT = ' + str(row[3]) + ', PROVDER_IN = ' + str(row[4]) + ', LOS_IN = ' + str(row[5]) + ', CAREPHASE = ' + str(row[6]) + ', DUMFLD1 = ' + str(row[7]) + ', DUMFLD2 = ' + str(row[8]) + ', DUMFLD3 = ' + str(row[9]) + ', DUMFLD4 = ' + str(row[10]) + ', DUMFLD5 = ' + str(row[11]) + ', DUMFLD6 = ' + str(row[12]) + ', DUMFLD7 = ' + str(row[13]) + ', DUMFLD8 = ' + str(row[14]) + ', DUMFLD9 = ' + str(row[15]) + ', DUMFLD10 = ' + str(row[16]) + ', DUMFLD11 = ' + str(row[17]) + ', DUMFLD12 = ' + str(row[18]) + ', DUMFLD13 = ' + str(row[19]) + ', DUMFLD14 = ' + str(row[20]) + ', DUMFLD15 = ' + str(row[21]) + ', DUMFLD16 = ' + str(row[22]) + ', DUMFLD17 = ' + str(row[23]) + ', DUMFLD18 = ' + str(row[24]) + ', DUMFLD19 = ' + str(row[25]) + ', DUMFLD20 = ' + str(row[26]) + ', INPT_SER = ' + str(row[27]) + ', ACCTNO = ' + str(row[28]),  + ', DE_STATUS = ' + str(row[29]),  + ', DECOMMFLAG = ' + str(row[30]) + ', PARENTID = ' + str(row[31]) + ', PARENTREC = ' + str(row[32]),  + ', COPYNO = ' + str(row[33]))
+			numberOfTuples = numberOfTuples + 1					#NOT READYY
+	except:
+		print ("ERROR: unable to fetch data")
+"""
 def filter():
 
-def output2File():"""
-
-
+def output2File():
+"""
 if __name__ == '__main__':
+	"""import pypyodbc
+	# Database connection
+	print('Trying to connect')
+	try:
+		myConnection = pypyodbc.connect('Driver={SQL Server};'
+									'Server=localhost;'
+									'Database=Trauma2;'
+									'uid=Rodolfo;pwd=Trauma2')
+		print('Connected')
+
+	except:
+		print('Could not connect')"""
+
+	class Tuples:
+		def __init__(self, patientId, task, timestamp, otherAttributes):
+			self.patientId = patientId
+			self.task = task
+			self.timestamp = timestamp
+			self.otherAttributes = otherAttributes
 	main()
